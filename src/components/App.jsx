7fbbe312831e0f892d53e6eaab76c0dde68e1fb0
fedorbor <112242236+fedorbor@users.noreply.github.com>
2023-12-23 13:18:10 +0200

    sd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@
 // import recipesData from './Recipe/RecipesData';
 
 import { Alert } from "./Alert/Alert";
-
+import { ColorPicker } from "./ColorPicker/ColorPicker";
+import { colorPickerOptions } from "./colorOptions"
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       {/* {recipesData.map((recipe, index) => (
         <Recipe key={index} recipe={recipe} />
       ))} */}
+      <ColorPicker options={colorPickerOptions}/>
       <Alert text="перестань" type="warning" />
       <Alert text="спрацювало" type="success" />
       <Alert text="error" type="error" />
--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,10 +1,12 @@
+import s from './ColorPicker.module.css';
+
 export const ColorPicker = ({options}) => {
     return (
-        <div>
-            <h2>Color Picker</h2>
+        <div className={s.container}>
+            <h2 className={s.title}>Color Picker</h2>
             {options.map(({label, color})=>(
-                <div>
-                    <span style={{backgroundColor: color}}>{label}</span>
+                <div className={s.option} style={{backgroundColor: color}}>
+                    <span>{label}</span>
                 </div>
             ))}
         </div>
