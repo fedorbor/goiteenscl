2ab9f234b46caeaff4616144f3c4eaab9eb77024
fedorbor <112242236+fedorbor@users.noreply.github.com>
2024-01-27 13:56:33 +0200

    k

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,37 @@
-import { StickersList } from "./Sticker/StickersList";
-import { Component } from "react";
-import stickerData from "../components/stickerData.json";
+// import { StickersList } from "./Sticker/StickersList";
+// import { Component } from "react";
+// import stickerData from "../components/stickerData.json";
 
-export class App extends Component {
-  state = {
-    label: '',
-  };
+// export class App extends Component {
+//   state = {
+//     label: '',
+//   };
 
-  handleChange = (newLabel) => {
-    this.setState({
-      label: newLabel,
-    });
-  };
+//   handleChange = (newLabel) => {
+//     this.setState({
+//       label: newLabel,
+//     });
+//   };
 
-  render() {
-    return (
-      <>
-        <StickersList
-          handleChange={this.handleChange}
-          stickerData={stickerData}
-        />
-      </>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <>
+//         <StickersList
+//           handleChange={this.handleChange}
+//           stickerData={stickerData}
+//         />
+//       </>
+//     );
+//   }
+// }
+import React from 'react';
+import ConferenceRegistration from './Regestration/ConferenceRegistration';
+
+export const App = () => {
+  return (
+    <div>
+      <ConferenceRegistration />
+    </div>
+  );
+};
+
