6ae280afb946bbd72d05f4b48cc74dd1d8aa9568
fedorbor <112242236+fedorbor@users.noreply.github.com>
2024-03-15 23:11:02 +0200

    asd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -244,14 +244,77 @@
 //     </div>
 //   );
 // };
-import React from 'react';
-import RegistrationForm from './RegistrationForm/RegistrationForm';
+// import React from 'react';
+// import RegistrationForm from './RegistrationForm/RegistrationForm';
 
-export function App() {
+// export function App() {
+//   return (
+//     <div>
+//       <RegistrationForm />
+//     </div>
+//   );
+// }
+
+import React, { useState } from 'react';
+import './App.css';
+import Section from './Feedback/Section';
+import Statistics from './Feedback/Statistics';
+import FeedbackOptions from './Feedback/FeedbackOptions';
+import Notification from './Feedback/Notification';
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = ['good', 'neutral', 'bad'];
+
+  const handleLeaveFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
+  const totalFeedback = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
   return (
     <div>
-      <RegistrationForm />
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={handleLeaveFeedback} />
+      </Section>
+
+      <Section title="Statistics">
+        {totalFeedback > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
     </div>
   );
-}
+};
 
